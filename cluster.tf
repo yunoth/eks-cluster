063fed4f11cfbe3d354941e4e9a63f287200d35d
root <root@devopscim1c.mylabserver.com>
2020-06-13 18:10:35 +0000

    bug fix

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "example" {
-  name     = "example"
+  name     = "eks-cluster"
   role_arn = "${aws_iam_role.example.arn}"
 
   vpc_config {
--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
   enable_nat_gateway = true
   tags = {
     Terraform = "true"
@@ -19,9 +18,15 @@ module "vpc" {
     "kubernetes.io/cluster/eks-cluster" = "shared"
   }
  public_subnet_tags = { 
-	"kubernetes.io/role/elb" = 1	
+    Terraform = "true"
+    Environment = "dev"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+    "kubernetes.io/role/elb" = 1	
  }
  private_subnet_tags = { 
-	"kubernetes.io/role/internal-elb" = 1	
+    Terraform = "true"
+    Environment = "dev"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb" = 1	
  }
 }
